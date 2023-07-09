fd444ce2f9971f97c01a9c40c4551e92926f29a2
unknown <melakualehegn34@gmail.com>
2023-07-09 15:28:21 +0300

    hero part animation in width

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,18 +7,17 @@ function Hero() {
         <div class="hero">
             <div class="hero-container">
                 <div class="hero-left">
-                    <h2>Have an audience?</h2>
-                    <a src='#'>I'm an INFLUENCER</a>
-                    <p>Find what's relevant to you and earn commissions
+                    <h1>Have an <br></br> audience?</h1>
+                    <a src='#' className='orange'>I'm an INFLUENCER</a>
+                    <p className='orange-text'>Find what's relevant to you and earn commissions
                         & rewards. Thousands of programs to choose from.</p>
-                    <img src={influencer} width={"200px"}></img>
+                    <img src={influencer} width={"440px"} className='left-image'></img>
                 </div>
                 <div class="hero-right">
-                    <h2>Have an affiliate program?</h2>
-                    <a src='#'>I'm a BUSINESS</a>
-                    <p>Enlist it here and let it promote itself for free.
+                    <h1>Have an <br></br>affiliate program?</h1>
+                    <a src='#' className='blue'>I'm a BUSINESS</a>
+                    <p className='blue-text'>Enlist it here and let it promote itself for free.
                         Make it relevant to thousands of influencers.</p>
-                    <img src={leftHero} width={"200px"}></img>
                 </div>
             </div>
         </div>
