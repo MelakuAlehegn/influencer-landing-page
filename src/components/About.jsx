883e18d983518955efd24afda6aae400c7149131
unknown <melakualehegn34@gmail.com>
2023-07-09 23:28:13 +0300

    main section css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,67 @@
 import React from 'react'
+import logo from "../assets/AR Logo2.png"
 
 function About() {
     return (
-        <div>about</div>
+        <div className='about'>
+            <div className='about-top'>
+                <div>
+                    <h1 className='span'><span>
+                        2000+
+                    </span><br></br>
+                        INFLUENCER <br></br> VISITS PER DAY
+                    </h1>
+                </div>
+                <div className='column'>
+                    <h1 className='span middle'><span>
+                        FREE
+                    </span><br></br>
+                        TO SIGNUP
+                    </h1>
+                    <h1 className='span middle'><span>
+                        EASY
+                    </span><br></br>
+                        TO USE
+                    </h1>
+                </div>
+                <div>
+                    <h1 className='span'><span>
+                        2000+
+                    </span><br></br>
+                        INLISTED <br></br> PROGRAMS
+                    </h1>
+                </div>
+            </div>
+            <div className='domian'>
+                <h1>affiliated<span>refer</span>.com</h1>
+                <img src={logo} width={'40px'}></img>
+            </div>
+            <div className='about-bottom'>
+                <div className='bottom-left'>
+                    <h1>ABOUT US</h1>
+                    <h2>CONTACT US</h2>
+                    <h2>USER SUPPORT</h2>
+                    <h2>FAQ</h2>
+                </div>
+                <div className='bottom-right'>
+                    <h1>BLOG POSTS</h1>
+                    <h1>FOLLOW US</h1>
+                    <div className='icons'>
+                        <a href="https://www.facebook.com"><i class="fab fa-facebook-f"></i></a>
+                        <a href="https://www.twitter.com"><i class="fab fa-twitter"></i></a>
+                        <a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a>
+                        <a href="https://www.pinterest.com"><i class="fab fa-pinterest"></i></a>
+                        <a href="https://www.youtube.com"><i class="fab fa-youtube"></i></a>
+                        <a href="https://www.linkedin.com/"><i class="fab fa-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+            <footer>
+                <a href='#'>TERMS OF USE</a>
+                <a href='#'>PRIVACY POLICY</a>
+                <p>&copy; 2023 affiliatedrefer.com</p>
+            </footer>
+        </div>
     )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,16 +4,16 @@ import leftHero from '../assets/pexels-pavel-danilyuk-5520327.png'
 
 function Hero() {
     return (
-        <div class="hero">
-            <div class="hero-container">
-                <div class="hero-left">
+        <div className="hero">
+            <div className="hero-container">
+                <div className="hero-left">
                     <h1>Have an <br></br> audience?</h1>
                     <a src='#' className='orange'>I'm an INFLUENCER</a>
                     <p className='orange-text'>Find what's relevant to you and earn commissions
                         & rewards. Thousands of programs to choose from.</p>
                     <img src={influencer} width={"440px"} className='left-image'></img>
                 </div>
-                <div class="hero-right">
+                <div className="hero-right">
                     <h1>Have an <br></br>affiliate program?</h1>
                     <a src='#' className='blue'>I'm a BUSINESS</a>
                     <p className='blue-text'>Enlist it here and let it promote itself for free.
