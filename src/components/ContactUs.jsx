077d1282fb23546588ced2b2daed8c6ba532ac6d
unknown <melakualehegn34@gmail.com>
2023-07-26 03:50:27 +0300

    background color change

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -7,39 +7,42 @@ function ContactUs() {
   return (
     <div className='cc'>
       <div className='contactus-page'>
-        <div className="contact-top">
-          <div className="contact-red">
+        <div className='para'>
+          <div className="contact-top">
+            <div className="contact-red">
 
-            <h3>Contact Us</h3>
-            <div className='new'>
-              <div>
-                <FaMapMarkerAlt />
-                <p>32, Avenue ve Newyork</p>
-              </div>
-              <div>
-                <FaEnvelope />
-                <p>hello@loremipsum.com</p>
-              </div>
-              <div>
-                <FaPhone />
-                <p>+3356 1589 2105</p>
-              </div>
-              <div>
-                <FaFax />
-                <p>+3356 1589 2105</p>
+              <h3>Contact Us</h3>
+              <div className='new'>
+                <div>
+                  <FaMapMarkerAlt />
+                  <p>32, Avenue ve Newyork</p>
+                </div>
+                <div>
+                  <FaEnvelope />
+                  <p>hello@loremipsum.com</p>
+                </div>
+                <div>
+                  <FaPhone />
+                  <p>+3356 1589 2105</p>
+                </div>
+                <div>
+                  <FaFax />
+                  <p>+3356 1589 2105</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="contact-white">
-            <div>
-              <h2>Get in Touch</h2>
-              <p>Feel free to drop us a line below!</p>
+            <div className="contact-white">
+              <div>
+                <h2>Get in Touch</h2>
+                <p>Feel free to drop us a line below!</p>
+              </div>
+              <input type="text" name="name" id="" placeholder='Your Name' />
+              <input type="email" name="email" id="" placeholder='Your Email' />
+              <textarea name="message" id="" cols="30" rows="8" placeholder='Type your message here...'></textarea>
+              <button type="submit">SEND</button>
             </div>
-            <input type="text" name="name" id="" placeholder='Your Name' />
-            <input type="email" name="email" id="" placeholder='Your Email' />
-            <textarea name="message" id="" cols="30" rows="8" placeholder='Type your message here...'></textarea>
-            <button type="submit">SEND</button>
           </div>
+          <p className='usr'>If you're seeking support as a user, please go to User Support.</p>
         </div>
       </div>
       <div className='about-bottom'>
